d6c7e2e2e11c19279691ae43cb2a6751df054201
Javier Escobar <jescobar7420@gmail.com>
2022-07-01 17:29:32 -0400

    Fix button position and add price product

--- src/app/interfaces/product.ts
+++ src/app/interfaces/product.ts
@@ -13,98 +13,98 @@ export const ListProducts: Array<Product> = [
       url: "https://www.sushitown.cl/images/cocacola1.5.png",
       marca: "Coca Cola",
       titulo: "Coca Cola Original 1.5 L",
-      precio: "Precio: $1870",
+      precio: "$1870",
       fecha: "01/02/2022"
     },{
       idproduct: 2,
       url: "https://jumbo.vtexassets.com/arquivos/ids/396797/Mantequilla-con-sal-250-g.jpg?v=637469298713900000",
       marca: "Colun",
       titulo: "Mantequilla con Sal",
-      precio: "Precio: $1990",
+      precio: "$1990",
       fecha: "01/01/2022"
     },{
       idproduct: 3,
       url: "https://i.imgur.com/WEnblNH.png",
       marca: "Natura",
       titulo: "Aceite de Maravilla 1 L",
-      precio: "Precio: $3959",
+      precio: "$3959",
       fecha: "01/01/2022"
     },{
       idproduct: 4,
       url: "https://i.imgur.com/nMPqNgY.png",
       marca: "Colun",
       titulo: "Manjar 400 g",
-      precio: "Precio: $1.729",
+      precio: "$1.729",
       fecha: "01/01/2022"
     },{
       idproduct: 5,
       url: "https://cdn.shopify.com/s/files/1/0513/3821/5592/products/WATTSDURAZNO_grande.png?v=1619719851",
       marca: "Watt's",
       titulo: "Jugo Durazno 1.5 L",
-      precio: "Precio: $1390",
+      precio: "$1390",
       fecha: "01/01/2022"
     },{
       idproduct: 6,
       url: "https://www.quepapita.com/389-medium_default/bebida-isotonica-cool-blue-gatorade-500-cc.jpg",
       marca:"Gatorade",
       titulo: "Bebida isotonica blue 1 L",
-      precio: "Precio: $1350",
+      precio: "$1350",
       fecha: "01/01/2022"
     },{
       idproduct: 3,
       url: "https://i.imgur.com/WEnblNH.png",
       marca: "Natura",
       titulo: "Aceite de Maravilla 1 L",
-      precio: "Precio: $3959",
+      precio: "$3959",
       fecha: "01/01/2022"
     },{
       idproduct: 4,
       url: "https://i.imgur.com/nMPqNgY.png",
       marca: "Colun",
       titulo: "Manjar 400 g",
-      precio: "Precio: $1.729",
+      precio: "$1.729",
       fecha: "01/01/2022"
     },{
       idproduct: 5,
       url: "https://cdn.shopify.com/s/files/1/0513/3821/5592/products/WATTSDURAZNO_grande.png?v=1619719851",
       marca: "Watt's",
       titulo: "Jugo Durazno 1.5 L",
-      precio: "Precio: $1390",
+      precio: "$1390",
       fecha: "01/01/2022"
     },{
       idproduct: 6,
       url: "https://www.quepapita.com/389-medium_default/bebida-isotonica-cool-blue-gatorade-500-cc.jpg",
       marca:"Gatorade",
       titulo: "Bebida isotonica blue 1 L",
-      precio: "Precio: $1350",
+      precio: "$1350",
       fecha: "01/01/2022"
     },{
       idproduct: 3,
       url: "https://i.imgur.com/WEnblNH.png",
       marca: "Natura",
       titulo: "Aceite de Maravilla 1 L",
-      precio: "Precio: $3959",
+      precio: "$3959",
       fecha: "01/01/2022"
     },{
       idproduct: 4,
       url: "https://i.imgur.com/nMPqNgY.png",
       marca: "Colun",
       titulo: "Manjar 400 g",
-      precio: "Precio: $1.729",
+      precio: "$1.729",
       fecha: "01/01/2022"
     },{
       idproduct: 5,
       url: "https://cdn.shopify.com/s/files/1/0513/3821/5592/products/WATTSDURAZNO_grande.png?v=1619719851",
       marca: "Watt's",
       titulo: "Jugo Durazno 1.5 L",
-      precio: "Precio: $1390",
+      precio: "$1390",
       fecha: "01/01/2022"
     },{
       idproduct: 6,
       url: "https://www.quepapita.com/389-medium_default/bebida-isotonica-cool-blue-gatorade-500-cc.jpg",
       marca:"Gatorade",
       titulo: "Bebida isotonica blue 1 L",
-      precio: "Precio: $1350",
+      precio: "$1350",
       fecha: "01/01/2022"
     }
 ]
